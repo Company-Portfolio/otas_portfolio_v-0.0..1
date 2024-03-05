fbbf6aa9625ee0b95f0224f2499ad5d0a9783d84
aaog2 <aungaungoogame2@gmail.com>
2024-03-05 21:51:04 +0630

    feat(init): :art: Project initiation for version 0.0.2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import AboutUsPage from "./pages/AboutUsPage";
-import WebServicePage from "./pages/WebServicePage";
-import AppServicePage from "./pages/AppServicePage";
-import FreeConsulatingPage from "./pages/FreeConsulatingPage";
-import { useEffect, useState } from "react";
-
 function App() {
-  const scrollToTop = () => {
-    window.scrollTo(0, 0);
-  };
-
-  return (
-    <>
-      <Router>
-        <Navbar scrollToTop={scrollToTop} />
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="/about" element={<AboutUsPage />} />
-          <Route path="/service">
-            <Route path="web" element={<WebServicePage />} />
-            <Route path="app" element={<AppServicePage />} />
-            <Route path="free" element={<FreeConsulatingPage />} />
-          </Route>
-        </Routes>
-      </Router>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
